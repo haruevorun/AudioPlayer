2b5bf2c8f62c628d2774715eb31b150fcd917f73
haruta-yamada <haruta_16n1101187@nnn.ed.jp>
2019-01-17 23:32:15 +0900

    add:AudioDataModel

---
+++ AudioPlayer/DataModel/AudioDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  AudioDataModel.swift
+//  AudioPlayer
+//
+//  Created by haruta yamada on 2019/01/17.
+//  Copyright © 2019 teranyan. All rights reserved.
+//
+
+import Foundation
+
+class AudioDataModel: NSObject {
+    
+}
